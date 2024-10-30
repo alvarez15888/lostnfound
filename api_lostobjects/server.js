9040c2373edc69f0aaf1c277c16d6abd75d6a9b7
Rodrigo Alvarez <alvarez15888@gmail.com>
2024-10-30 17:42:11 +0100

    "api"

---
+++ api_lostobjects/server.js
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import app from "./app.js";
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+//https://aplicaciones.aragon.es/pfi_priv/signedDocumentFolder.action
